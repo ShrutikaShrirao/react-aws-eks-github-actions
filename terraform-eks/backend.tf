405ad4da2f17f07c5960ab1f7e8e601d803fb59a
Vaishnaviisasre <131643394+Vaishnaviisasre@users.noreply.github.com>
2024-01-25 15:06:47 +0530

    Update backend.tf

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "codewithmuh" # Replace with your actual S3 bucket name
+    bucket = "codewithmuhreact" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-central-1"
   }
