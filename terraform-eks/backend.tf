f529e516df7b7efaf663a2f96fd2df5bc8ce87f2
ShrutikaShrirao <100130865+ShrutikaShrirao@users.noreply.github.com>
2024-02-01 11:29:46 +0530

    Update backend.tf

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "codewithmuhreact" # Replace with your actual S3 bucket name
+    bucket = "shrutikashrirao" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
